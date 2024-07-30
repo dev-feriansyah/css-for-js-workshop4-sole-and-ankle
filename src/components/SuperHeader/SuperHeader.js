045750d4820339f24706020a9a6a240977d8b1af
Feriansyah <devferiansyah@gmail.com>
2024-07-30 16:24:55 +0800

    feat: modify superheader for exercise 1

--- src/components/SuperHeader/SuperHeader.js
+++ src/components/SuperHeader/SuperHeader.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
+import { COLORS } from '../../constants'
 
-import SearchInput from '../SearchInput';
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
+import SearchInput from '../SearchInput'
+import UnstyledButton from '../UnstyledButton'
+import Icon from '../Icon'
 
 const SuperHeader = () => {
   return (
@@ -19,18 +19,22 @@ const SuperHeader = () => {
         <Icon id="shopping-bag" strokeWidth={1} />
       </UnstyledButton>
     </Wrapper>
-  );
-};
+  )
+}
 
 const Wrapper = styled.div`
   font-size: 0.875rem;
   color: ${COLORS.gray[300]};
   background-color: ${COLORS.gray[900]};
-`;
+  display: flex;
+  gap: 24px;
+  padding: 8px 32px;
+`
 
 const MarketingMessage = styled.span`
   color: ${COLORS.white};
-`;
+  margin-right: auto;
+`
 
 const HelpLink = styled.a`
   color: inherit;
@@ -40,6 +44,6 @@ const HelpLink = styled.a`
   &:not(:focus-visible) {
     outline: none;
   }
-`;
+`
 
-export default SuperHeader;
+export default SuperHeader
